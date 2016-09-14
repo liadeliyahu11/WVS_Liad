03973f55cb14e42db169c2b8565646f2afc324cb
liadeliyahu11 <liadeliyahu11@gmail.com>
2016-09-14 15:22:45 +0300

    Add files via upload

---
+++ links.py
@@ -0,0 +1,19 @@
+import requests
+import re
+import os
+url = raw_input("please enter url:")
+ans = requests.get(url)
+html = ans.text
+links = re.findall("href=\"([^\"]*)\"",html)
+forms = re.findall("<form [^>]* action=\"([^\"]*)\"",html)
+parameters = re.findall("<input [^>]* name=\"([^\"]*)\"",html)
+for i in links:
+	print i
+print "\n\nparameters:\n\n"
+for i in parameters:
+	print i
+print "\n\nforms:\n\n"
+for i in forms:
+	print i
+print len(forms)
+os.system("pause")
