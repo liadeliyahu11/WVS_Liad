ca8b8eea420246c5962afce416632ef290469431
liadeliyahu11 <liadeliyahu11@gmail.com>
2016-09-17 17:00:20 +0300

    Update webCrawler.py

--- webCrawler.py
+++ webCrawler.py
@@ -2,10 +2,21 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import os
-parameters,forms,final_parameters = [],[],[]
+
+cluesForError = ["404","not found","Not Found","Not found","was not found on this server","The requested URL","ErrorDocument to handle the request"]
 
 
+def notFound(html):
+	c = 0
+	for i in cluesForError:
+		if i in html:
+			c += 1
+	if c>3:
+		return True
+	return False
+	
 def createFormsList(html):
+	parameters,final_parameters = [],[]
 	parsed_html = BeautifulSoup(html,'html.parser')
 	for par in parsed_html.find_all('input'):
 		parameters.append((par.get('name'),str(par)))
@@ -25,12 +36,15 @@ def existInFile(fileName,toFind):
 			return True
 	return False
 
+
 def scanPage(filename,url):
 	try:
 		if url[-1] == "/":
 			url = url[:-1]
 		ans = requests.get(url)
 		html = ans.text
+		if notFound(html):
+			return False
 		parameters = createFormsList(html)
 		f = open(filename+".txt",'a+')
 		links = re.findall("href=\"([^\"]*)\"",html)
@@ -47,29 +61,27 @@ def scanPage(filename,url):
 		f.close()
 		return True
 	except Exception as ex:
-		print ex
 		return False
 
 def scanAllPages(url):
 	scanPage(url[7:],url)
 	f = open(url[7:]+".txt",'r')
+	filename = url[7:]
 	lines = f.readlines()
 	f.close()
 	for i in lines:
-		if not scanPage(url[7:],url+"/"+i[:-1]):
-			print "first:"+url+"/"+i[:-1]
-			if not scanPage(url[7:],url+i[-1]):
-				print "second:"+url+i[-1]
-				if not scanPage(url[7:],i[-1]):
-					print "third:"+url+i[-1]
+		i = i[:-1] #without \n from the file
+		if not scanPage(filename,url+"/"+i):
+			if not scanPage(filename,url+i):
+				if not scanPage(filename,i):
 					f = open("errors.txt","a+")
 					f.write(i+"\n")
 					f.close()
 
 def main():
 	url = raw_input("please enter url:")
-	#scanPage(url,url)
-	scanAllPages(url)
+	#scanAllPages(url)
+	print notFound(requests.get(url).text)
 	os.system("pause")
 	
 if __name__ == "__main__":
