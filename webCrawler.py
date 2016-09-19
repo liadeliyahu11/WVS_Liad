87172fbc008445381a0222224807c9aee9f90694
liadeliyahu11 <liadeliyahu11@gmail.com>
2016-09-19 17:35:31 +0300

    Update webCrawler.py

--- webCrawler.py
+++ webCrawler.py
@@ -5,11 +5,17 @@ import threading
 import os
 import sys  
 
-cluesForError = ["404","not found","Not Found","Not found","was not found on this server","The requested URL","ErrorDocument to handle the request"]
+MAX_DEPTH = 5
+MAX_THREADS = 1000
+cluesForError = ["The resource you are looking","had its name changed","or is temporarily unavailable","File or directory not found","404","not found","Not Found","Not found","was not found on this server","The requested URL","ErrorDocument to handle the request"]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
 }
-cv = threading.Lock()
+threads = []
+allLinks = []
+total = []
+
+
 
 def notFound(html):
 	c = 0
@@ -41,90 +47,98 @@ def existInFile(fileName,toFind):
 			return True
 	return False
 
-def scanPage(filename,url,page):
-	if not scanOnePage(filename,url,page):
-		f = open("errors.txt","a+")
-		f.write(page+"\n")
-		f.close()
+def scanPage(filename,url,page,depth):
+	if not scanOnePage(filename,url,page,depth):
+		allLinks.remove(page)
 		return False
 	return True
 
-def linkValidator(url,i):
+def linkValid(url,i):
+	"""
+	return true if valid
+	"""
 	if i==url or i==url[7:] or ((i[:7]=="http://" or i[:8]=="https://") and 
-		((url[7:] not in i) or (url[7:-1] not in i) or (url[8:] not in i) or (url[8:-1] not in i))):
+		((url not in i or url[:7]+"www."+url[7:] not in i) or (url[:-1] not in i or url[:7]+"www."+url[7:-1] not in i)
+		or (url not in i or url[:8]+"www."+url[8:] not in i) or (url[:-1] not in i or url[:8]+"www."+url[8:-1] not in i))):
 		return False
 	return True
 
-
-def scanOnePage(filename,url,page):
-	try:
-		base = url
-		ans = requests.get(url+"/"+page,headers=headers)
+def linkExist(url,page):
+	if page[:4] == "http":
+		ans = requests.get(page,headers=headers)
 		html = ans.text.encode('utf-8')
 		if ans.status_code == 404 and notFound(html):
-			ans = requests.get(page,headers=headers)
+			ans = requests.get(url+"/"+page,headers=headers)
 			html = ans.text.encode('utf-8')
 			if ans.status_code == 404 and notFound(html):
 				return False
 			else:
-				url = page
+				url = url+"/"+page
 		else:
-			url = url+"/"+page
+			url = page
+	else:
+		ans = requests.get(url+"/"+page,headers=headers)
+		html = ans.text.encode('utf-8')
+		if ans.status_code == 404 and notFound(html):
+			return False
+		else:
+				url = url+"/"+page
+	return (html,url)
+
+def scanOnePage(filename,url,page,depth):
+	try:
+		my_threads = []
+		base = url
+		html,url = linkExist(url,page) # if not exist exception will raise
 		parameters = createFormsList(html)
-		f = open(filename+".txt",'a+')
 		links = re.findall("href=\"([^\"]*)\"",html)
+		total.append(url)
 		for i in links:
-			if not existInFile(filename+".txt",i) and linkValidator(base,i):#doesnt exist and doesnt equal to the this url
-				#cv.acquire()
-				f.write(i.encode('utf-8')+"\n")
-				#cv.release()
-		f.close()
+			if i.encode('utf-8') not in allLinks and linkValid(base,i):#doesnt exist and doesnt equal to this url
+				allLinks.append(i)
+				if depth <=MAX_DEPTH:
+					if len(threads)<=MAX_THREADS:
+						t = threading.Thread(target=scanOnePage,args=(filename,url,i,depth+1))
+						threads.append(t)
+						my_threads.append(t)
+						t.start()
+		for i in my_threads:
+			i.join()
 		f = open(filename+"-forms.txt",'a+')
 		tagOpen = False
 		for i in parameters:
 			if not existInFile(filename+"-forms.txt",str(i).encode('utf-8')):
-				#cv.acquire()
 				if not tagOpen:
 					f.write("url:"+"\n"+url+"\n")
 					tagOpen = True
 				f.write(str(i).encode('utf-8')+"\n")
-				#cv.release()
-		#cv.acquire()
 		if tagOpen:
 			f.write("endUrl\n")
-		#cv.release()
 		f.close()
 		return True
 	except Exception as ex:
-		print ex
-		return False
+		pass
+	return False
 
 def scanAllPages(url):
-	threads = []
 	if url[:5] == "https":
 		filename = url[8:].replace('/','-')
 	else:
 		filename = url[7:].replace('/','-')
-	if scanPage(filename,url,""):
-		f = open(filename+".txt",'r')
-		lines = f.readlines()
+	if scanPage(filename,url,"",0):
+		print "scan started..."
+		f = open(filename+".txt","w")
+		for i in total:
+			f.write(i+"\n")
 		f.close()
-		f = open(filename+".txt",'w')
-		f.write("\n")
-		f.close()
-		for i in lines:
-			i = i[:-1] #without \n from the file
-			t = threading.Thread(target=scanPage,args=(filename,url,i))
-			threads.append(t)
-			t.start()
-						
+		print "scan completed!"
 	else:
 		print "cant scan the page you gave"
 
 def main():
 	reload(sys)  
 	sys.setdefaultencoding('utf8')
-	url = raw_input("please enter url:")
+	url = raw_input("please enter url for example:\n http://some.com\nurl:")
 	scanAllPages(url)
 	os.system("pause")
 	
